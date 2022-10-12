aae043c13c3546cd68a05c99f7575e18c5745655
dsetzer8 <dsetzer8@uncc.edu>
2022-10-12 15:45:08 -0400

    added routes

--- Project2/app.js
+++ Project2/app.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
-const storyRoutes = require('./routes/storyRoutes');
+const connectionRoutes = require('./routes/connectionRoutes');
+
 //Create App
 const app = express();
 
 //Configure App
-let port = 3000;
+let port = 8084;
 let host = 'localhost';
 app.set('view engine', 'ejs');
 
@@ -17,10 +18,16 @@ app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
 
-//Routes Setup
+//Main Route
 app.get('/', (req,res)=>{
     res.render('index');
 });
+
+//Connection Route
+app.use('/connections', connectionRoutes);
+
+//Error Handlers
+
 
 //Start Server
 app.listen(port, host, ()=>{
---
+++ Project2/routes/connectionRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const router = express.Router();
+
+//GET /connections: Send all connections to the user
+router.get('/', (req,res)=>{
+    res.send('Send All Connections');
+});
+
+//GET /connections/newConnection: Send html form for creating a new connection
+router.get('/newConnection', (req,res)=>{
+    res.send('Send New Connections Form');
+});
+
+//POST /connections: Create a new connection
+router.post('/', (req,res)=>{
+    res.send('Created A New Connection');
+});
+
+//GET /connections/:id: Send details of connection identified by id
+router.get('/:id', (req,res)=>{
+    res.send('Send Connection With ID ' + req.params.id);
+});
+
+//GET /connections/:id/edit: Send html form for editing an existing story
+router.get('/:id/edit', (req,res)=>{
+    res.send('Send The Edit Form ');
+});
+
+//PUT /connections/:id: Update the connection identified by id
+router.put('/:id', (req,res)=>{
+    res.send('Update Story With Id ' + req.params.id);
+});
+
+//DELETE /connections/:id: Delete the story identified by id
+router.delete('/:id', (req,res)=>{
+    res.send('Delete Story With Id ' + req.params.id);
+});
+
+module.exports = router;
