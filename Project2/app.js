53e932abad5b1b62075c620b61edec8a781dc6cb
dsetzer8 <dsetzer8@uncc.edu>
2022-10-12 14:48:32 -0400

    app.js update

---
+++ Project2/app.js
@@ -0,0 +1,25 @@
+//Required Modules
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const storyRoutes = require('./routes/storyRoutes');
+//Create App
+const app = express();
+
+//Configure App
+let port = 3000;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+//Mount Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+//Routes Setup
+
+//Start Server
+app.listen(port, host, ()=>{
+    console.log('Server is running on port ', port);
+})
