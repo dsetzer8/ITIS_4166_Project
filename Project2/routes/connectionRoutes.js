a4e20d649f272db72e0151e832b8e07b9873d29a
dsetzer8 <dsetzer8@uncc.edu>
2022-10-12 15:58:40 -0400

    udpates Routes

--- Project2/routes/connectionRoutes.js
+++ Project2/routes/connectionRoutes.js
@@ -4,38 +4,24 @@ const controller = require('../controller/connectionController');
 const router = express.Router();
 
 //GET /connections: Send all connections to the user
-router.get('/', (req,res)=>{
-    res.send('Send All Connections');
-});
+router.get('/', controller.index);
 
 //GET /connections/newConnection: Send html form for creating a new connection
-router.get('/newConnection', (req,res)=>{
-    res.send('Send New Connections Form');
-});
+router.get('/newConnection', controller.new);
 
 //POST /connections: Create a new connection
-router.post('/', (req,res)=>{
-    res.send('Created A New Connection');
-});
+router.post('/', controller.create);
 
 //GET /connections/:id: Send details of connection identified by id
-router.get('/:id', (req,res)=>{
-    res.send('Send Connection With ID ' + req.params.id);
-});
+router.get('/:id', controller.show);
 
 //GET /connections/:id/edit: Send html form for editing an existing story
-router.get('/:id/edit', (req,res)=>{
-    res.send('Send The Edit Form ');
-});
+router.get('/:id/edit',controller.edit);
 
 //PUT /connections/:id: Update the connection identified by id
-router.put('/:id', (req,res)=>{
-    res.send('Update Story With Id ' + req.params.id);
-});
+router.put('/:id', controller.update);
 
 //DELETE /connections/:id: Delete the story identified by id
-router.delete('/:id', (req,res)=>{
-    res.send('Delete Story With Id ' + req.params.id);
-});
+router.delete('/:id', controller.delete);
 
 module.exports = router;
